96ffc21598888fb77309ac0ceeb5824a82793647
Francisco <fcontreras18@gmail.com>
2016-09-23 01:07:01 +0000

    Form submission

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Nomster::Application.routes.draw do
   devise_for :users
+  root 'places#index'
+  resources :places do
+    resources :comments, only: :create
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -13,7 +17,6 @@ Nomster::Application.routes.draw do
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
-  resources :places
 
   # Example resource route with options:
   #   resources :products do
@@ -54,7 +57,6 @@ Nomster::Application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-  root 'places#index'
 end
 
 
